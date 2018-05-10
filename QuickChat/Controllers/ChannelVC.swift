1ccc997a4e8bafba490328c6a0fb51d06b434a5f
thanhbh <thanhbh@admins-iMac.local>
2018-05-10 16:09:54 +0700

    add framework

---
+++ QuickChat/Controllers/ChannelVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelVC.swift
+//  QuickChat
+//
+//  Created by thanhbh on 5/10/18.
+//  Copyright © 2018 vinicorp. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
