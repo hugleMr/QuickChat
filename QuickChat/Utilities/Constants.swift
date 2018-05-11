119eb58b0a106d55623d324c64fb364ded343d8c
thanhbh <thanhbh@admins-imac.local>
2018-05-11 17:11:27 +0700

    add constants

---
+++ QuickChat/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  QuickChat
+//
+//  Created by thanhbh on 5/11/18.
+//  Copyright © 2018 vinicorp. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLoginSegue"
+let CHANNEL_CELL = "channelCell"
