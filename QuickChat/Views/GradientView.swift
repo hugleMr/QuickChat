c0a47e64cb6c155a17808200a9bb3f0820c7a214
thanhbh <thanhbh@admins-imac.local>
2018-05-11 14:00:19 +0700

    update gradient view

--- QuickChat/Views/GradientView.swift
+++ QuickChat/Views/GradientView.swift
@@ -11,21 +11,33 @@ import UIKit
 @IBDesignable
 class GradientView: UIView {
     
-    @IBInspectable var topColor: UIColor = UIColor.blue {
+    @IBInspectable var resolution: Int = 1 {
         didSet {
             self.setNeedsLayout()
         }
     }
     
-    @IBInspectable var bottomColor: UIColor = UIColor.green {
-        didSet {
-            self.setNeedsLayout()
-        }
-    }
+//    @IBInspectable var topColor: UIColor = UIColor.red {
+//        didSet {
+//            self.setNeedsLayout()
+//        }
+//    }
+//
+//    @IBInspectable var bottomColor: UIColor = UIColor.purple {
+//        didSet {
+//            self.setNeedsLayout()
+//        }
+//    }
     
     override func layoutSubviews() {
+        let subdivisions:CGFloat = CGFloat(256 / (resolution == 0 ? 1 : resolution))
         let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.colors = []
+        for i in 0...255 {
+            let pointColor: CGColor = UIColor(hue: CGFloat(i)/subdivisions, saturation: 1, brightness: 1, alpha: 1).cgColor
+            gradientLayer.colors?.append(pointColor)
+        }
+//        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         gradientLayer.frame = self.bounds
