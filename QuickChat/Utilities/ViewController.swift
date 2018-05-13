d610134c64ad602400ff1648ed5b04e1c497cc58
Thanh Huy Bùi <thanh@thanhs-mbp.net.fpt>
2018-05-13 17:24:22 +0700

    add view controller extension

---
+++ QuickChat/Utilities/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by Bùi Huy Thanh on 5/13/18.
+//  Copyright © 2018 vinicorp. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @objc func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+}
