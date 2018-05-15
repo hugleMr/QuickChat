868c0dc11f27ec938f0ef78cd29359c55b32a1ce
thanhbh <thanhbh@admins-imac.local>
2018-05-15 15:14:13 +0700

    replace close action

--- QuickChat/Controllers/SignUpVC.swift
+++ QuickChat/Controllers/SignUpVC.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class SignUpVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtRePass: UITextField!
+    @IBOutlet weak var imgUser: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let tapGestureBackground = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped(_:)))
         self.view.addGestureRecognizer(tapGestureBackground)
+    }
+    
+    @IBAction func btnCreateAccountPressed(_ sender: UIButton) {
+        guard let email = txtEmail.text, txtEmail.text != "" else {
+            return
+        }
+        guard let pass = txtPassword.text, txtPassword.text != "" else {
+            return
+        }
+        guard let rePass = txtRePass.text else {
+            return
+        }
+        if rePass != pass {
+            return
+        }
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                self.view.makeToast("User has been registered!")
+            }
+        }
+    }
+    
+    @IBAction func btnClosePressed(_ sender: UIButton) {
+        performSegue(withIdentifier: TO_CHANNEL, sender: nil)
+    }
+    
+    @IBAction func btnChooseAvatarPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnGenerateBackground(_ sender: UIButton) {
+        
     }
 }
