13f1e454b0f7529d5668ca2835050964d3a58e42
thanhbh <thanhbh@admins-imac.local>
2018-05-16 15:03:56 +0700

    update AvatarCollectionView

--- QuickChat/Views/AvatarCollectionView.swift
+++ QuickChat/Views/AvatarCollectionView.swift
@@ -11,18 +11,19 @@ import UIKit
 @IBDesignable
 class AvatarCollectionView: UICollectionView {
     
-    @IBInspectable var numberOfCellsPerRow: CGFloat = 4 {
+    
+    @IBInspectable var cellsPerRow: UInt = 4 {
         didSet {
             self.setNeedsLayout()
         }
     }
     
     fileprivate func customizedView() {
+        let item = CGFloat(cellsPerRow)
         if let flowLayout = self.collectionViewLayout as? UICollectionViewFlowLayout {
             let horizontalSpacing = flowLayout.scrollDirection == .vertical ? flowLayout.minimumInteritemSpacing : flowLayout.minimumLineSpacing
             flowLayout.sectionInset = UIEdgeInsetsMake(0, horizontalSpacing / 2, 0, horizontalSpacing / 2)
-            let cellWidth = (self.bounds.width - max(0, numberOfCellsPerRow - 1) * horizontalSpacing - horizontalSpacing) / numberOfCellsPerRow
-            
+            let cellWidth = (self.bounds.width - max(0, item - 1) * horizontalSpacing - horizontalSpacing) / item
             flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
         }
     }
