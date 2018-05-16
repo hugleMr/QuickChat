d60cef0f476475409eaf0d1ea25a74f1211b98e1
thanhbh <thanhbh@admins-imac.local>
2018-05-16 14:45:33 +0700

    add AvatarCell

---
+++ QuickChat/Views/AvatarCell.swift
@@ -0,0 +1,35 @@
+//
+//  AvatarCell.swift
+//  QuickChat
+//
+//  Created by thanhbh on 5/16/18.
+//  Copyright © 2018 vinicorp. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgAvatar: UIImageView!
+    
+    fileprivate func customizedView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+//    override func layoutSubviews() {
+//        customizedView()
+//    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizedView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        customizedView()
+    }
+
+}
