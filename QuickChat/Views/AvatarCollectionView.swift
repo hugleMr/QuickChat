1a8a01b6374045db5e7bf6b02530f24cc65942fe
Thanh Huy Bùi <thanh@thanhs-macbook-pro.local>
2018-05-16 22:07:52 +0700

    update avatar collection view

--- QuickChat/Views/AvatarCollectionView.swift
+++ QuickChat/Views/AvatarCollectionView.swift
@@ -15,21 +15,21 @@ class AvatarCollectionView: UICollectionView {
     @IBInspectable
     var numberColumns: UInt = 4 {
         didSet {
-            if numberColumns != 0 {
-                customizedView()
-            }
+            customizedView()
             self.setNeedsLayout()
         }
     }
     
     fileprivate func customizedView() {
-        if let flowLayout = self.collectionViewLayout as? UICollectionViewFlowLayout {
-            let horizontalSpacing = flowLayout.scrollDirection == .vertical ? flowLayout.minimumInteritemSpacing : flowLayout.minimumLineSpacing
-            let sectionInset = horizontalSpacing
-            let cellsPerRow = CGFloat(numberColumns)
-            flowLayout.sectionInset = UIEdgeInsetsMake(0, sectionInset, 0, sectionInset)
-            let cellWidth = (self.bounds.width - max(0, cellsPerRow - 1) * horizontalSpacing - sectionInset * 2) / cellsPerRow
-            flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        if numberColumns != 0 {
+            if let flowLayout = self.collectionViewLayout as? UICollectionViewFlowLayout {
+                let horizontalSpacing = flowLayout.scrollDirection == .vertical ? flowLayout.minimumInteritemSpacing : flowLayout.minimumLineSpacing
+                let sectionInset = horizontalSpacing
+                let cellsPerRow = CGFloat(numberColumns)
+                flowLayout.sectionInset = UIEdgeInsetsMake(0, sectionInset, 0, sectionInset)
+                let cellWidth = (self.bounds.width - max(0, cellsPerRow - 1) * horizontalSpacing - sectionInset * 2) / cellsPerRow
+                flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+            }
         }
     }
     
@@ -41,7 +41,7 @@ class AvatarCollectionView: UICollectionView {
 //        super.awakeFromNib()
 //        customizedView()
 //    }
-//
+
 //    override func prepareForInterfaceBuilder() {
 //        super.prepareForInterfaceBuilder()
 //        customizedView()
