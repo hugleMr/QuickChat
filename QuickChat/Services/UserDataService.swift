b88ef3a6fa4ae0da35339b558e2de480f9f27879
thanhbh <thanhbh@admins-imac.local>
2018-05-16 14:43:58 +0700

    change user data service

--- QuickChat/Services/UserDataService.swift
+++ QuickChat/Services/UserDataService.swift
@@ -12,17 +12,24 @@ import Alamofire
 class UserDataService {
     
     static let instance = UserDataService()
-    public private(set) var id = ""
-    public private(set) var name = ""
-    public private(set) var email = ""
-    public private(set) var avatarName = ""
-    public private(set) var avatarColor = ""
+
+//    public private(set) var id = ""
+//    public private(set) var name = ""
+//    public private(set) var email = ""
+//    public private(set) var avatarName = ""
+//    public private(set) var avatarColor = ""
+//
+//    func setUserData(userData: UserData) {
+//        self.id = userData.id
+//        self.name = userData.name
+//        self.email = userData.email
+//        self.avatarName = userData.avatarName
+//        self.avatarColor = userData.avatarColor
+//    }
+    
+    public private(set) var userData = UserData()
     
     func setUserData(userData: UserData) {
-        self.id = userData.id
-        self.name = userData.name
-        self.email = userData.email
-        self.avatarName = userData.avatarName
-        self.avatarColor = userData.avatarColor
+        self.userData = userData
     }
 }
