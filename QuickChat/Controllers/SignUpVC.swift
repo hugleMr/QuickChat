4bdde0dc764108ef9a066d63291e5973e032244d
thanhbh <thanhbh@admins-imac.local>
2018-05-16 14:49:53 +0700

    update SignUp view controller

--- QuickChat/Controllers/SignUpVC.swift
+++ QuickChat/Controllers/SignUpVC.swift
@@ -18,6 +18,9 @@ class SignUpVC: UIViewController {
     @IBOutlet weak var txtRePass: UITextField!
     @IBOutlet weak var imgUser: UIImageView!
     
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,25 +29,39 @@ class SignUpVC: UIViewController {
     }
     
     @IBAction func btnCreateAccountPressed(_ sender: UIButton) {
+        guard let name = txtUsername.text, txtUsername.text != "" else {
+            return
+        }
         guard let email = txtEmail.text, txtEmail.text != "" else {
             return
         }
         guard let pass = txtPassword.text, txtPassword.text != "" else {
             return
         }
-        guard let rePass = txtRePass.text else {
+        guard let rePass = txtRePass.text, txtRePass.text != "" else {
             return
         }
         if rePass != pass {
             return
         }
-        AuthService.instance.registerUser(user: User(email, pass)) { (success) in
+        //AuthService.instance.registerUser(user: User(email, pass)) { (success) in
+        //    if success {
+        //        self.dismiss(animated: true, completion: nil)
+        //    } else {
+        //        print("User cannot be registered!")
+        //    }
+        //}
+        AuthService.instance.createUser(name: name, email: email, avatarName: avatarName, avatarColor: avatarColor) { (success) in
             if success {
-                self.dismiss(animated: true, completion: nil)
+                self.performSegue(withIdentifier: TO_CHANNEL, sender: nil)
             } else {
                 print("User cannot be created!")
             }
         }
+    }
+    
+    @IBAction func btnBackPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func btnClosePressed(_ sender: UIButton) {
@@ -52,10 +69,9 @@ class SignUpVC: UIViewController {
     }
     
     @IBAction func btnChooseAvatarPressed(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: TO_AVATAR, sender: nil)
     }
     
     @IBAction func btnGenerateBackground(_ sender: UIButton) {
-        
     }
 }
