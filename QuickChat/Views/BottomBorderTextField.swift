8daace2a4086682e4d61cf63961bfcc08102eb5c
thanhbh <thanhbh@admins-imac.local>
2018-05-16 16:42:57 +0700

    simplize code

--- QuickChat/Views/BottomBorderTextField.swift
+++ QuickChat/Views/BottomBorderTextField.swift
@@ -10,14 +10,21 @@ import UIKit
 
 @IBDesignable
 class BottomBorderTextField: UITextField {
+    
+    @IBInspectable
+    var bottomBorderWidth: CGFloat = 2.0 {
+        didSet {
+            customizedView()
+            self.setNeedsLayout()
+        }
+    }
 
     fileprivate func customizedView() {
         let border = CALayer()
-        let width = CGFloat(2.0)
+        border.borderWidth = bottomBorderWidth
         border.borderColor = #colorLiteral(red: 0.3568627451, green: 0.6235294118, blue: 0.7960784314, alpha: 1)
-        border.borderWidth = width
         border.cornerRadius = 5.0
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        border.frame = CGRect(x: 0, y: self.frame.size.height - border.borderWidth, width:  self.frame.size.width, height: self.frame.size.height)
         self.borderStyle = .none
         self.layer.addSublayer(border)
         self.layer.masksToBounds = true
@@ -27,13 +34,13 @@ class BottomBorderTextField: UITextField {
 //        customizedView()
 //    }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        customizedView()
-    }
-    
-    override func prepareForInterfaceBuilder() {
-        customizedView()
-    }
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        customizedView()
+//    }
+//
+//    override func prepareForInterfaceBuilder() {
+//        customizedView()
+//    }
 
 }
