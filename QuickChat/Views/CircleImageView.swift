0b8fc1d5005005ec1efc17666ebbf03521efaf73
thanhbh <thanhbh@admins-imac.local>
2018-05-17 10:02:06 +0700

    add circle image view

---
+++ QuickChat/Views/CircleImageView.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImageView.swift
+//  QuickChat
+//
+//  Created by thanhbh on 5/17/18.
+//  Copyright © 2018 vinicorp. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImageView: UIImageView {
+
+    override func awakeFromNib() {
+        self.setRounded()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        self.prepareForInterfaceBuilder()
+        self.setRounded()
+    }
+
+}
