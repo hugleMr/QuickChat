67e7fe2e743ebae576b63d1d7babccd39db70674
thanhbh <thanhbh@admins-imac.local>
2018-05-17 17:21:53 +0700

    add property errorMessage, method findUserByEmail and optimized code

--- QuickChat/Services/AuthService.swift
+++ QuickChat/Services/AuthService.swift
@@ -15,21 +15,30 @@ class AuthService {
     static let instance = AuthService()
     let defaults = UserDefaults.standard
     
-    var isLoggedIn: Bool {
-        get {
-            return defaults.bool(forKey: LOGGED_IN_KEY)
-        }
-        set {
-            defaults.set(newValue, forKey: LOGGED_IN_KEY)
-        }
-    }
-    
     var authToken: String? {
         get {
             return defaults.value(forKey: TOKEN_KEY) as? String
         }
         set {
             defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var errorMessage: String? {
+        get {
+            return defaults.value(forKey: ERROR_MESSAGE) as? String
+        }
+        set {
+            defaults.set(newValue, forKey: ERROR_MESSAGE)
+        }
+    }
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
         }
     }
     
@@ -79,16 +88,31 @@ class AuthService {
                     return
                 }
                 let json = JSON(data)
-                self.userEmail = json["user"].stringValue
-                self.authToken = json["token"].stringValue
-                
-                self.isLoggedIn = true
+                if json["message"].exists() {
+                    self.errorMessage = json["message"].stringValue
+                } else if json["user"].exists() && json["token"].exists() {
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                    self.isLoggedIn = true
+                }
                 completion(true)
             } else {
                 self.isLoggedIn = false
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
+        }
+    }
+    
+    fileprivate func setUserData(_ data: Data) {
+        let json = JSON(data)
+        if json["_id"].exists() && json["name"].exists() && json["email"].exists() && json["avatarName"].exists() && json["avatarColor"].exists() {
+            let id = json["_id"].stringValue
+            let name = json["name"].stringValue
+            let email = json["email"].stringValue
+            let avatarName = json["avatarName"].stringValue
+            let avatarColor = json["avatarColor"].stringValue
+            UserDataService.instance.setUserData(userData: UserData(id: id, name: name, email: email, avatarName: avatarName, avatarColor: avatarColor))
         }
     }
     
@@ -102,22 +126,38 @@ class AuthService {
         ]
         if let authToken = self.authToken {
             let header = [
-                "Authorization": "Bearer \(authToken)"
+                "Authorization": "Bearer \(authToken)",
+                "Content-Type": "application/json; charset=utf-8"
             ]
-            Alamofire.request(URL_ADD_USER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header.merging(HEADER_API, uniquingKeysWith: { (oldValue, newValue) -> String in
-                return newValue
-            })).responseJSON { (response) in
+            Alamofire.request(URL_ADD_USER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
                 if response.result.error == nil {
                     guard let data = response.data else {
                         return
                     }
-                    let json = JSON(data)
-                    let id = json["_id"].stringValue
-                    let name = json["name"].stringValue
-                    let email = json["email"].stringValue
-                    let avatarName = json["avatarName"].stringValue
-                    let avatarColor = json["avatarColor"].stringValue
-                    UserDataService.instance.setUserData(userData: UserData(id: id, name: name, email: email, avatarName: avatarName, avatarColor: avatarColor))
+                    self.setUserData(data)
+                    completion(true)
+                } else {
+                    completion(false)
+                    debugPrint(response.result.error as Any)
+                }
+            }
+        } else {
+            completion(false)
+        }
+    }
+    
+    func findUserByEmail(completion: @escaping CompletionHandle) {
+        if let authToken = self.authToken {
+            let header = [
+                "Authorization": "Bearer \(authToken)",
+                "Content-Type": "application/json; charset=utf-8"
+            ]
+            Alamofire.request("\(URL_USER_BY_EMAIL)\(userEmail)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+                if response.result.error == nil {
+                    guard let data = response.data else {
+                        return
+                    }
+                    self.setUserData(data)
                     completion(true)
                 } else {
                     completion(false)
